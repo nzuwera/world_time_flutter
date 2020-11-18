c5ecb77af4fb7197ab12b3a5da51ff20c91ff8c1
nzuwera <nzuwera2002@gmail.com>
2020-11-18 12:46:44 +0200

    initial commit

---
+++ android/app/src/main/java/com/nzuwera/android/world_time/MainActivity.java
@@ -0,0 +1,6 @@
+package com.nzuwera.android.world_time;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/services/world_time.dart
@@ -0,0 +1,40 @@
+import 'dart:convert';
+
+import 'package:http/http.dart';
+import 'package:intl/intl.dart';
+
+class WorldTime {
+  String location; // location name for the UI
+  String time; // time in that location
+  String flag; // url to an asset flag icon
+  String url; // location url for api endpoint
+  bool isDaytime;
+
+  WorldTime({this.location, this.flag, this.url});
+
+  Future<void> getTime() async {
+    try {
+      // Make request
+      Response response =
+          await get('http://worldtimeapi.org/api/timezone/$url');
+      Map data = jsonDecode(response.body);
+      print(data);
+
+      String dateTime = data['utc_datetime'];
+      String offSet = data['utc_offset'].substring(1, 3);
+      // print(dateTime);
+      // print(data['utc_offset']);
+
+      DateTime now = DateTime.parse(dateTime);
+      now = now.add(Duration(hours: int.parse(offSet)));
+      // set time property
+      // print('now.hour : ${now.hour}');
+      isDaytime = now.hour > 6 && now.hour < 20 ? true : false;
+      time = DateFormat.jm().format(now);
+    } catch (e) {
+      print('Caught error: $e');
+      time = 'Could not requested data';
+      isDaytime = true;
+    }
+  }
+}
